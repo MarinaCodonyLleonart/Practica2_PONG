eee178f52ffa6bd1d8ffb03c49db619e99dafbba
MarinaCodonyLleonart <mcodony@edu.tecnocampus.cat>
2021-10-11 18:37:54 +0200

    TODO 8

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local w, h -- Variables to store the screen width and height
 
 local ballX, ballY -- Variables to store the position of the ball in the screen (Uncomment at the start of TODO 6)
---local ballSpeed -- Variable to store the ball speed (Uncomment at the start of TODO 8)
+local ballSpeed -- Variable to store the ball speed (Uncomment at the start of TODO 8)
 --local playerX, playerY, cpuX, cpuY -- Variables to store the position of the player and cpu paddle (Uncomment at the start of TODO 10)
 --local paddleSpeed -- Variable to store the paddle speed (Uncomment at the start of TODO 12)
 --local ballAngle -- Variable to estore the ball movement angle (Uncomment at the start of TODO 16)
@@ -18,6 +18,7 @@ function love.load(arg)
   -- TODO 6: Initialize the position of the ball at the center of the screen
   
   -- TODO 8: Initialize the ball speed for going to the left
+  ballSpeed = -20
   
   -- TODO 10: Initialize the player and cpu paddles position
   
