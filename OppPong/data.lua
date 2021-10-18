81b73375f7c2e2f003c9ca4322f7d69414ffe7dd
Elisabeth Alzueta <ealzueta@edu.tecnocampus.cat>
2021-10-18 17:50:03 +0200

    data variables

--- OppPong/data.lua
+++ OppPong/data.lua
@@ -1,17 +1,16 @@
 local data = Object:extend()
+local w, h -- Variables to store the screen width and height
 
+local ballX, ballY -- Variables to store the position of the ball in the screen (Uncomment at the start of TODO 6)
+local ballRadius
+local ballSpeed -- Variable to store the ball speed (Uncomment at the start of TODO 8)
 
+local playerX, playerY, cpuX, cpuY -- Variables to store the position of the player and cpu paddle (Uncomment at the start of TODO 10)
+local paddleWidth 
+local paddleHeight 
+local paddleSpeed -- Variable to store the paddle speed (Uncomment at the start of TODO 12)
 
-
-
-
-
-
-
-
-
-
-
-
+local ballAngle -- Variable to estore the ball movement angle (Uncomment at the start of TODO 16)
+local playerPoints, cpuPoints -- Variable to store the player and cpu points (Uncomment at the start of TODO 21)
 
 return data
