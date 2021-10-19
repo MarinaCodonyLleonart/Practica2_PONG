3c038946c73ce2d71114d67f4ffb3903198c3ec6
Elisabeth Alzueta <ealzueta@edu.tecnocampus.cat>
2021-10-19 11:26:34 +0200

    oppPong2 v2 no va

--- OppPong2/src/ball.lua
+++ OppPong2/src/ball.lua
@@ -2,21 +2,18 @@ local Actor = Actor or require "src/actor" --1: Find file of father class
 local Cball = Actor:extend() --2: make the hierarchy
 
 local Vector = Vector or require "src/vector"
+local vector = Vector()
 
 local Cmain = Cmain or require "main" --is used in BallCollisionsPaddle()
 local main = Cmain()
 local Cdata = Cdata or require "data"
 local d = Cdata()
 
---local bx, by
---local bSpeed
---local bAngle
-
 function Cball:new(x, y)
-    --Actor:new(image,x,y,speed,fx,fy)
-  Cball.super:new("Resources/flower.png",d.ballX,d.ballY,d.ballBaseSpeed,1,0)
+  --Actor:new(image,x,y,speed,fx,fy)
+  Cball.super:new("Resources/flowerBall.png",d.ballX,d.ballY,d.ballBaseSpeed,1,0)
 
- -- self.position = Vector.new(d.ballX, d.ballY)
+ -- self.position = vector.new(d.ballX, d.ballY)
   --bx, by = x, y or d.ballX, d.ballY
  -- self.speed = d.ballBaseSpeed
   --self.rot = d.ballAngle
@@ -41,7 +38,7 @@ function Cball:update(dt)
   
 end
 
-function Cball.draw()
+function Cball:draw()
   local xx = self.position.x
   local ox = self.origin.x
   local yy = self.position.y
@@ -53,8 +50,8 @@ function Cball.draw()
 
 --ADDITIONAL BALL FUNCTIONS
 function ResetBall()
-      bX, bY = d.w/2, d.h/2
-      bSpeed = ballBaseSpeed 
+      self.position= vector.new(d.w/2, d.h/2)
+      self.speed = d.ballBaseSpeed 
     end
 
 function BallCollisionsPaddle()
@@ -63,12 +60,7 @@ function BallCollisionsPaddle()
    self.rot = -(self.rot - math.pi/2) + math.pi/2
   end
   
- -- if (self.position.y>playerY and self.position.y<playerY+d.paddleHeight and bX<playerX+paddleWidth)
- --  or (self.position.y>cpuY and self.position.y<cpuY+d.paddleHeight and bX>cpuX-d.paddleWidth) then
- -- bAngle = -(bAngle - math.pi/2) + math.pi/2
- -- self.speed = self.speed + bSpeed*paddleAcc
- --end
- --if (bY>playerY and bY<self.position.Y+d.paddleHeight and bX<playerX+paddleWidth)
+ --if (bY>playerY and bY<playerY+d.paddleHeight and bX<playerX+paddleWidth)
  --  or (bY>cpuY and bY<cpuY+d.paddleHeight and bX>cpuX-d.paddleWidth) then
  --  bAngle = -(bAngle - math.pi/2) + math.pi/2
  --  self.speed = self.speed + bSpeed*paddleAcc
@@ -77,7 +69,6 @@ function BallCollisionsPaddle()
 end
 
 function BallCollisionsScreen() --Superior and inferior parts of the screen
-  
   if self.position.y<0 or self.position.y>d.h then
     self.rot = -(-self.rot - math.pi/4) + math.pi/4
   end
