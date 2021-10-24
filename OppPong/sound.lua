c53df36ca883c39f68cbea5ee1f957e1eafe329c
Elisabeth Alzueta <ealzueta@edu.tecnocampus.cat>
2021-10-24 19:21:49 +0200

    deleted opp opp2

--- OppPong/sound.lua
+++
@@ -1,9 +0,0 @@
-local Object = Object or require "lib/classic"
-local SoundLib = Object:extend()
-
-function SoundLib:new()
-  ballCollision = love.audio.newSource("resources/grass_sound.wav", "static")
-  scoring = love.audio.newSource("resources/ItemPickup.wav", "static")
-end
-
-return SoundLib
